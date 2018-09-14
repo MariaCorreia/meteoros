2cabd82cf2de782a16c97735a6d1a3e869b05452
Maria Correia <mvocorreia@hotmail.com>
2018-09-14 14:51:21 -0300

    added commented-out flow of main program

--- main.cpp
+++ main.cpp
@@ -45,25 +45,63 @@ void update(sf::RenderWindow &window, sf::Time dt){
 	//.run() all objects on the screen
 }
 
+void checkColision(){
+	
+	//meteor-vector.getMeteorPositions(); 
+	//rocket.getPositions();
+	//house.getPositions();	
+
+	//for every meteor in meteor-vector
+		//if (rocket collides with meteor)
+			//meteor-vector.delete(meteor index)
+			//rocket.delete()
+		
+		//if (meteor collides with house) {
+			//house.delete();
+			//meteor-vector.delete(meteor index)
+			//scoreboard++;
+		//}
+		
+		
+			
+	
+}
 
 //-----------------------------------------------------------------------
 int main() {
 	sf::RenderWindow window(sf::VideoMode(500, 500), "Meteoros");
 
+	//initialization
+	
+	//initializes meteor-vector
+	//initializes houses
+	//initializes rocket launcher
+	
 	sf::Clock clock;
 	sf::Time now = clock.getElapsedTime();
 	while (window.isOpen()) {
 		now = clock.restart(); //restarts clock every iteraction,stores time since last restart, aka, dt
 
 		window.clear();
-		Pause(16); //60 updates per second, aka 60fps
 
+		//if (interval between meteors has passed)
+			//meteor-vector.createMeteor();
+		
+		//meteor-vector.update(); //updates meteor position
+		//rocket.update(); //updates rocket position
+		
+		//checkColision(); //checks collision between meteor and rocket, meteor and house, and deletes if necessary
+		// updateScoreboard(); //updates passage of time on scoreboard
+		
 		input(window);
 		show (window);
 		update(window, now);
 
 		
 		window.display(); //plasters everything that has bem .draw()'ed into the screen
+		Pause(16); //60 updates per second, aka 60fps
+
+	
 	}
 
 	return 0;
